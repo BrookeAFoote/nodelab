916425f37d57cd9a1f2c3bdffa1d649f3ee45ede
BrookeAFoote <bfoote@tulane.edu>
2018-02-22 14:27:15 -0800

    it works

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var tasks = 
+var tasks = [
+    "Adopt a Bambino.",
+    "Cook dinner",
+    "Pack a lunch",
+]
+
+function randomString(array){
+    var randomNumber = Math.floor(Math.random()*array.length);
+    return array[randomNumber];
+}//randomstring
+
+module.exports = tasks
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // server.js
 var http = require("http");
-
+var tasks = require("./main");
+function randomString(array){
+    var randomNumber = Math.floor(Math.random()*array.length);
+    return array[randomNumber];
+}//randomstring
 http.createServer(function(request, response) {
 response.writeHead(200, { "Content-type": "text/plain" });
-response.write("I'm tired and done.");
+response.write(randomString(tasks));
 response.end();
 }).listen(3000);
+
